59946e2bed946103954b8cf86a063045f5a4f4d0
Kyle Pullen <kyle.pullen@knights.ucf.edu>
2022-08-10 01:30:10 -0400

    Added 5 round functionality

--- logic.js
+++ logic.js
@@ -1,5 +1,6 @@
 game();
 
+
 function getComputerChoice() {
     choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() * choices.length)];
@@ -16,27 +17,52 @@ function computerSelection() {
 function playRound() {
     playerChoice = playerSelection();
     computerChoice = computerSelection();
+    var winner;
 
     console.log(`Player choice: ${playerChoice}`);
     console.log(`Computer choice: ${computerChoice}`);
     //Draw
     if (playerChoice === computerChoice) {
         console.log('You picked the same choice as the computer - Draw!')
+        return;
     } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
-        console.log('Winner winner chicken dinner!')
+        console.log('Winner! Rock beats scissors!')
+        return winner = 'player';
     } else if (playerChoice === 'rock' && computerChoice === 'paper') {
         console.log('You Lose! Paper beats Rock');
+        return winner = 'computer';
     } else if (playerChoice === 'paper' && computerChoice === 'scissors') {
         console.log('You Lose! Scissors beats Paper!');
+        return winner = 'computer';
     } else if (playerChoice === 'paper' && computerChoice === 'rock') {
-        console.log('Winner winner chicken dinner!');
+        console.log('Winner! Paper beats rock!');
+        return winner = 'player';
     } else if (playerSelection === 'scissors' && computerChoice === 'paper') {
-        console.log('Winner winner chicken dinner');
+        console.log('Winner scissors beats paper!');
+        return winner = 'player';
     } else {
-        console.log('You lose!  Rock beats scissors');
+        console.log('You lose! Rock beats scissors');
+        return winner = 'computer';
     }
 }
 
 function game() {
-    playRound();
+    playerWins = 0;
+    computerWins = 0;
+
+    for (let i = 1; i <= 5; i++) {
+        console.log(`Round ${i}`);
+        if (playRound() === 'player') {
+            playerWins++;
+            console.log(`Player wins: ${playerWins}, Computer wins: ${computerWins}`);
+            console.log('----------------------------------');
+            continue;
+        } else {
+            computerWins++;
+            console.log(`Player wins: ${playerWins}, Computer wins: ${computerWins}`);
+            console.log('----------------------------------');
+        }
+    }
+
+
 }
